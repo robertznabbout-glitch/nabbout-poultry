890d71038b8b15c64abb0d440ec4a1dffa1f0d33
robertznabbout-glitch <robertznabbout@gmail.com>
2026-08-06 23:48:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".button");
 
-    buttons.forEach(function (button) {
-        button.addEventListener("click", function () {
-            button.classList.add("clicked");
-
-            setTimeout(function () {
-                button.classList.remove("clicked");
-            }, 250);
-        });
-    });
-});
